b755601db3a2659d2bd609ac1b069c1b8885fd11
GeevesH-IS <hugo.geeves@informed.com>
2018-02-15 17:32:03 +0000

    Added Django Cron Job formatting, time between runs now an internal setting

--- application/automatic_deletion.py
+++ application/automatic_deletion.py
@@ -1,19 +1,21 @@
 from application import models
+from data_coordinator import settings
 from datetime import datetime, timedelta
 
-from django_cron import CronJobBase
+from django_cron import CronJobBase, Schedule
 
 
-class MyCronJob(CronJobBase):
+class automatic_deletion(CronJobBase):
 
-    def automatic_deletion():
+    RUN_EVERY_MINS = settings.AUTOMATIC_DELETION_FREQUENCY
+
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'application.automatic_deletion'
+
+    def do(self):
         ninety_days_ago = datetime.now() - timedelta(days=90)
-
         test_model = list(models.Application.objects.filter(date_updated__lte=ninety_days_ago))
         for model in test_model:
-            print('Deleting application: ' + str(model.pk))
+            print(str(datetime.now()) + 'Deleting application: ' + str(model.pk))
             model.delete()
 
-
-    if __name__ == 'main':
-        automatic_deletion()
