06e3636ca428473e0e679fc0fde9a6c804e03302
GeevesH-IS <hugo.geeves@informed.com>
2018-02-15 15:24:34 +0000

    Added data_coordinator app, currently deletes all 90 applications when certain URL pinged

---
+++ application/automatic_deletion.py
@@ -0,0 +1,16 @@
+
+from application import models
+from datetime import datetime, timedelta
+
+
+def automatic_deletion():
+
+    ninety_days_ago = datetime.now() - timedelta(days=90)
+
+    test_model = list(models.Application.objects.filter(date_updated__lte=ninety_days_ago))
+    for model in test_model:
+        print('Deleting application: ' + str(model.pk))
+        model.delete()
+
+if __name__ == 'main':
+    automatic_deletion()
