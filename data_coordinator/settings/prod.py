8203ba9ac52f5c34116f0370f3f4d31f5ec65c96
Roman Gorodeckij <holms@holms.lt>
2018-02-23 18:02:11 +0000

    data_coordinator: settings split
    Affects #34

---
+++ data_coordinator/settings/prod.py
@@ -0,0 +1,32 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# You should never enable this in production, even if it's temporarily
+# All INSTALLED_APPS in django relies on this variable, like google-analytics app.
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+PROD_APPS = [
+]
+
+INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': os.environ.get('DATABASE_USER', 'ofsted'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'OfstedB3ta'),
+        'HOST': os.environ.get('DATABASE_HOST', '130.130.52.132'),
+        'PORT': os.environ.get('DATABASE_PORT', '5462')
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'l^fle)2fs3k6r2aqmh)v1@$1#=cs5v28n*+=0z=4el067i6&pf'
