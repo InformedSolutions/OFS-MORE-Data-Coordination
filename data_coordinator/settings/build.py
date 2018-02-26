9946a82bf943072563ce038bec990a82d8276217
Cruddas <james.cruddas@informed.com>
2018-02-26 21:49:56 +0000

    settings: Add build settings file

---
+++ data_coordinator/settings/build.py
@@ -0,0 +1,33 @@
+from .base import *
+
+DEBUG = True
+
+INTERNAL_IPS = "127.0.0.1"
+
+
+DEV_APPS = [
+  'debug_toolbar'
+]
+
+MIDDLEWARE_DEV = [
+  'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': os.environ.get('DATABASE_USER', 'ofsted'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'OfstedB3ta'),
+        'HOST': os.environ.get('DATABASE_HOST', 'ofsted-postgres'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432')
+    }
+}
+
+MIDDLEWARE = MIDDLEWARE + MIDDLEWARE_DEV
+INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + DEV_APPS + PROJECT_APPS
+
+SECRET_KEY = '-asdasdsad322432maq#j23432*&(*&DASl6#mhak%8rbh$px8e&9c6b9@c7df=m'
