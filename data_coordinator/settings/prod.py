d1a8a2902e743073170b494d0c2f7e46527328cd
Roman Gorodeckij <holms@holms.lt>
2018-02-28 18:14:45 +0000

    settings: changed db env vars name

--- data_coordinator/settings/prod.py
+++ data_coordinator/settings/prod.py
@@ -20,13 +20,12 @@ INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': os.environ.get('DATABASE_USER', 'ofsted'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'OfstedB3ta'),
-        'HOST': os.environ.get('DATABASE_HOST', '130.130.52.132'),
-        'PORT': os.environ.get('DATABASE_PORT', '5462')
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'ofsted'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'OfstedB3ta'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'ofsted-postgres'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432')
     }
 }
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'l^fle)2fs3k6r2aqmh)v1@$1#=cs5v28n*+=0z=4el067i6&pf'
