de70f623f77454407721e90c82414b9838ea15d2
Rica Duchateau <rica.duchateau@informed.com>
2018-07-03 09:41:13 +0100

    Try-catch

--- application/delayed_email.py
+++ application/delayed_email.py
@@ -18,43 +18,46 @@ class DelayedEmail(CronJobBase):
         """
         Function for sending a reminder email detailing next steps to an applicant
         """
-        log = logging.getLogger('django.server')
-        log.info('Checking for applications that have been accepted 10 days ago')
-        send_next_steps = find_accepted_applications()
-        log.info(send_next_steps)
+        try:
+            log = logging.getLogger('django.server')
+            log.info('Checking for applications that have been accepted 10 days ago')
+            send_next_steps = find_accepted_applications()
+            log.info(send_next_steps)
 
-        for send in send_next_steps:
-            application = Application.objects.get(pk=send.application_id)
-            log.info(application.application_id)
+            for send in send_next_steps:
+                application = Application.objects.get(pk=send.application_id)
+                log.info(application.application_id)
 
-            if ApplicantPersonalDetails.objects.filter(application_id=application).count() > 0:
-                applicant = ApplicantPersonalDetails.objects.get(application_id=application)
-                applicant_name_record = ApplicantName.objects.get(personal_detail_id=applicant)
-                log.info(applicant_name_record)
-                applicant_name = applicant_name_record.first_name
-            else:
-                applicant_name = 'applicant'
+                if ApplicantPersonalDetails.objects.filter(application_id=application).count() > 0:
+                    applicant = ApplicantPersonalDetails.objects.get(application_id=application)
+                    applicant_name_record = ApplicantName.objects.get(personal_detail_id=applicant)
+                    log.info(applicant_name_record)
+                    applicant_name = applicant_name_record.first_name
+                else:
+                    applicant_name = 'applicant'
 
-            log.info(applicant_name)
-            log.info(str(datetime.now()) + ' - Sending next steps: ' + str(send.pk))
-            template_id = '3de3b404-64fc-49a1-b01f-1d0607760c60'
-            email = UserDetails.objects.get(application_id=application).email
-            log.info(email)
-            base_url = settings.PUBLIC_APPLICATION_URL
-            log.info(base_url)
-            documents_needed = base_url + '/documents-needed?id=' + str(send.application_id)
-            log.info(documents_needed)
-            home_ready = base_url + '/home-ready?id=' + str(send.application_id)
-            prepare_interview = base_url + '/prepare-interview?id=' + str(send.application_id)
-            personalisation = {"ref": str(send.application_reference),
-                               "first_name": applicant_name,
-                               "documents_needed": documents_needed,
-                               "home_ready": home_ready,
-                               "prepare_interview": prepare_interview}
+                log.info(applicant_name)
+                log.info(str(datetime.now()) + ' - Sending next steps: ' + str(send.pk))
+                template_id = '3de3b404-64fc-49a1-b01f-1d0607760c60'
+                email = UserDetails.objects.get(application_id=application).email
+                log.info(email)
+                base_url = settings.PUBLIC_APPLICATION_URL
+                log.info(base_url)
+                documents_needed = base_url + '/documents-needed?id=' + str(send.application_id)
+                log.info(documents_needed)
+                home_ready = base_url + '/home-ready?id=' + str(send.application_id)
+                prepare_interview = base_url + '/prepare-interview?id=' + str(send.application_id)
+                personalisation = {"ref": str(send.application_reference),
+                                   "first_name": applicant_name,
+                                   "documents_needed": documents_needed,
+                                   "home_ready": home_ready,
+                                   "prepare_interview": prepare_interview}
 
-            log.info(personalisation)
-            r = send_email(email, personalisation, template_id)
-            log.info(r)
-            send.ofsted_visit_email_sent = datetime.now()
-            send.save()
-            log.info(send.ofsted_visit_email_sent)
+                log.info(personalisation)
+                r = send_email(email, personalisation, template_id)
+                log.info(r)
+                send.ofsted_visit_email_sent = datetime.now()
+                send.save()
+                log.info(send.ofsted_visit_email_sent)
+        except:
+            print('Error')
