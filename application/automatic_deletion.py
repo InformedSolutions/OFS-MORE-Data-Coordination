8c281e93ca6043e1a16c98027840d95fe7340064
Rica Duchateau <rica.duchateau@informed.com>
2018-09-27 14:47:36 +0100

    Linting of file

--- application/automatic_deletion.py
+++ application/automatic_deletion.py
@@ -8,7 +8,6 @@ from django_cron import CronJobBase, Schedule
 
 
 class automatic_deletion(CronJobBase):
-
     RUN_EVERY_MINS = settings.AUTOMATIC_DELETION_FREQUENCY
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
@@ -18,11 +17,10 @@ class automatic_deletion(CronJobBase):
         log = logging.getLogger('django.server')
         log.info('Checking for expired applications')
         ninety_days_ago = datetime.now() - timedelta(days=90)
-        expired_submissions = list(Application.objects.exclude(application_status='COMPLETE').filter(date_last_accessed__lte=ninety_days_ago))
+        expired_submissions = list(
+            Application.objects.exclude(application_status='COMPLETE').filter(date_last_accessed__lte=ninety_days_ago))
 
         for submission in expired_submissions:
             print(str(datetime.now()) + ' - Deleting application: ' + str(submission.pk))
             log.info(str(datetime.now()) + ' - Deleting application: ' + str(submission.pk))
             submission.delete()
-
-
