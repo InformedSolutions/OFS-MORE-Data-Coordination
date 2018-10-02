230d9c808d77491d50ff02d0432771833cc42f8c
Rica Duchateau <rica.duchateau@informed.com>
2018-10-02 10:28:30 +0100

    Temporarily set application expiry threshold to 1 day

--- data_coordinator/settings/test.py
+++ data_coordinator/settings/test.py
@@ -4,6 +4,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Automatic deletion frequency in minutes
+AUTOMATIC_DELETION_FREQUENCY = 2
+
+# Expiry threshold for applications in days
+EXPIRY_THRESHOLD = 1
+
 DEV_APPS = [
     'debug_toolbar',
 ]
