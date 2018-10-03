d79e7314fd3cb2b540d9d6dcc1812f8be6c17769
Rica Duchateau <rica.duchateau@informed.com>
2018-10-03 14:17:23 +0100

    Align Application model with Childminder

--- application/models/application.py
+++ application/models/application.py
@@ -69,9 +69,9 @@ class Application(models.Model):
     application_reference = models.CharField(blank=True, null=True, max_length=9,
                                              validators=[RegexValidator(r'(\w{2})([0-9]{7})')])
     ofsted_visit_email_sent = models.DateTimeField(blank=True, null=True)
-    publish_details = models.NullBooleanField(blank=True, null=True, default=None)
     working_in_other_childminder_home = models.NullBooleanField(blank=True, null=True, default=None)
     own_children = models.NullBooleanField(blank=True, null=True, default=None)
+    own_children_not_in_home = models.NullBooleanField(blank=True, null=True, default=None)
 
     @classmethod
     def get_id(cls, app_id):
