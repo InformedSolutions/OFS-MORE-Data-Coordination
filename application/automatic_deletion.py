ce2b4d1d93ae99e6ee71fbcc28ad77d2de931ec2
jenniferbrown94 <jennifer.brown@informed.com>
2019-07-17 17:30:35 +0100

    error logging

--- application/automatic_deletion.py
+++ application/automatic_deletion.py
@@ -24,60 +24,67 @@ class AutomaticDeletion(CronJobBase):
     code = 'application.automatic_deletion'
 
     def do(self):
-        log.info('Checking for expired  and expiring applications')
+        with utils.CronErrorContext() as error_context:
+            log.info('Checking for expired  and expiring applications')
 
-        self._childminder_expiry_warnings()
-        self._childminder_deletions()
+            with error_context.sub():
+                self._childminder_expiry_warnings(error_context)
+            with error_context.sub():
+                self._childminder_deletions(error_context)
 
-    def _childminder_expiry_warnings(self):
+    def _childminder_expiry_warnings(self, error_context):
 
         send_reminder_cm = generate_expiring_applications_list_cm_applications()
 
         for cm_application in send_reminder_cm:
+            with error_context.sub():
 
-            if cm_application.application_expiry_email_sent:
-                log.debug('Already sent')
-                continue
+                if cm_application.application_expiry_email_sent:
+                    log.debug('Already sent')
+                    continue
 
-            log.info(str(datetime.now()) + ' - Sending reminder email: ' + str(cm_application.pk))
-            log.info(cm_application.application_id)
+                log.info(str(datetime.now()) + ' - Sending reminder email: ' + str(cm_application.pk))
+                log.info(cm_application.application_id)
 
-            template_id = 'b52414b8-afb4-4b6b-8b10-d87efa2714f4'
-            user = UserDetails.objects.get(application_id=cm_application)
-            try:
-                name_model = ApplicantName.objects.get(application_id=cm_application)
-            except ApplicantName.DoesNotExist:
-                # applicant might not have got as far as completing personal details
-                name_model = None
-            if name_model and name_model.first_name:
-                applicant_first_name = name_model.first_name
-            else:
-                applicant_first_name = 'applicant'
-            base_url = settings.CHILDMINDER_EMAIL_VALIDATION_URL
-            user.magic_link_email = ''.join([random.choice(string.ascii_letters + string.digits)
-                                             for n in range(12)])
-            user.magic_link_email = user.magic_link_email.upper()
-            # "expiry date" is actually creation time as epoch seconds
-            user.email_expiry_date = int(time.time())
-            log.info((user, base_url, user.magic_link_email))
-            personalisation = {"link": base_url + '/validate/' + user.magic_link_email,
+                template_id = 'b52414b8-afb4-4b6b-8b10-d87efa2714f4'
+                user = UserDetails.objects.get(application_id=cm_application)
+                try:
+                    name_model = ApplicantName.objects.get(application_id=cm_application)
+                except ApplicantName.DoesNotExist:
+                    # applicant might not have got as far as completing personal details
+                    name_model = None
+                if name_model and name_model.first_name:
+                    applicant_first_name = name_model.first_name
+                else:
+                    applicant_first_name = 'applicant'
+                base_url = settings.CHILDMINDER_EMAIL_VALIDATION_URL
+                user.magic_link_email = ''.join([random.choice(string.ascii_letters + string.digits)
+                                                 for n in range(12)])
+                user.magic_link_email = user.magic_link_email.upper()
+                # "expiry date" is actually creation time as epoch seconds
+                user.email_expiry_date = int(time.time())
+                log.info((user, base_url, user.magic_link_email))
+                personalisation = {"link": base_url + '/validate/' + user.magic_link_email,
                                    "first_name": applicant_first_name}
-            log.info(personalisation['link'])
-            r = send_email(user.email, personalisation, template_id, service_name='Childminder')
-            log.info(r)
-            if r.status_code not in (200, 201):
-                raise ConnectionError(r.status_code)
-            cm_application.application_expiry_email_sent = True
-            cm_application.save()
-            user.save()
+                log.info(personalisation['link'])
+                r = send_email(user.email, personalisation, template_id, service_name='Childminder')
+                log.info(r)
+                if r.status_code not in (200, 201):
+                    raise ConnectionError(r.status_code)
+                cm_application.application_expiry_email_sent = True
+                cm_application.save()
+                user.save()
+
 
     def _childminder_deletions(self, error_context):
 
         expired_cm_applications = generate_list_of_expired_cm_applications()
 
         for cm_application in expired_cm_applications:
-            log.info(str(datetime.now()) + ' - Deleting application: ' + str(cm_application.pk))
+            with error_context.sub():
+                log.info(str(datetime.now()) + ' - Deleting application: ' + str(cm_application.pk))
 
-            # Delete Application, with the deletion of associated records handled by on_delete=models.CASCADE in the
-            # ForeignKey
-            cm_application.delete()
+                # Delete Application, with the deletion of associated records handled by on_delete=models.CASCADE in the
+                # ForeignKey
+                cm_application.delete()
+
