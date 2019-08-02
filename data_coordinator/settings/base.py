2251802876db63a9e440602d354fc8453818a1c3
katej321 <klj525@york.ac.uk>
2019-08-02 15:49:12 +0100

    Expiry increase

--- data_coordinator/settings/base.py
+++ data_coordinator/settings/base.py
@@ -22,7 +22,7 @@ AUTOMATIC_DELETION_FREQUENCY = 1
 NEXT_STEPS_EMAIL_DELAY_IN_DAYS = os.environ.get('NEXT_STEPS_EMAIL_DELAY_IN_DAYS', 0.0001)
 
 # Expiry threshold for applications in days
-CHILDMINDER_EXPIRY_THRESHOLD = float(os.environ.get('CHILDMINDER_EXPIRY_THRESHOLD', 90))
+CHILDMINDER_EXPIRY_THRESHOLD = float(os.environ.get('CHILDMINDER_EXPIRY_THRESHOLD', 110))
 
 # Warning before expiry email threshold for applications
 WARNING_EMAIL_THRESHOLD_DAYS = float(os.environ.get('WARNING_EMAIL_THRESHOLD', 70))
